b651bc849d598f55ad29402d4e60e8e310d9cc63
ewansou <ewansou@hotmail.com>
2014-03-08 01:56:07 +0800

    changed hastag to picobunny

--- subscribe.php
+++
@@ -1,9 +0,0 @@
-<?php
-$client_id = 'f0ba06d272b14a9684be7544addb413e';
-$client_secret = 'b12a9368332b4a63aa2c38328757c819';
-$object = 'tag';
-$object_id = 'ootd';
-$aspect = 'media';
-$verify_token='';
-$callback_url = 'http://instagram-real-time.herokuapp.com/callback.html';
-?>
